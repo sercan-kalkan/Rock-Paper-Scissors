d047c098d6d945f5ca270fe5cb33aa85ec7d0941
Sercan Kalkan <sercankalkan1@gmail.com>
2023-04-29 23:42:38 +0300

    getting player choice and function for computers choice has done.

---
+++ main.js
@@ -0,0 +1,20 @@
+//getting choice of user
+let userChoice  = prompt("Rock, Paper or Scissors").toLocaleLowerCase;
+
+
+//getComputerChoice
+let computerChoice= function getComputerChoice () {
+    const choice = ["rock", "paper", "scissors"];
+    const random = Math.floor(Math.random() * choice.length);
+    return choice[random];
+}
+console.log(computerChoice())
+
+//function playing the game
+function playGame(userChoice,computerChoice ) {
+ 
+    if
+
+
+}
+
