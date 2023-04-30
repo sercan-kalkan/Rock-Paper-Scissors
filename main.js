04146dfd8b3a01e83fa70dab98f753fdfbd17166
Sercan Kalkan <sercankalkan1@gmail.com>
2023-05-01 01:29:55 +0300

    playRound function has been created

--- main.js
+++ main.js
@@ -1,34 +1,36 @@
-//getting choice of user
-let userChoice  = prompt("Rock, Paper or Scissors").toLocaleLowerCase;
-
+let playerSelection  = prompt("Rock, Paper or Scissors").toLowerCase();
 
 //getComputerChoice
-let computerChoice= function getComputerChoice () {
+let  getComputerChoice = function () {
     const choice = ["rock", "paper", "scissors"];
     const random = Math.floor(Math.random() * choice.length);
-    return choice[random.toString()];
+    return choice[random];
 }
 
+const computerSelection = getComputerChoice();
 
-//function playing the game
-function playGame(userChoice,computerChoice ) {
-    switch(userChoice) {
-        case "rock":
-            if (computerChoice = "scissors") {
-                console.log("You win")
-            }
-            break;
-        case "paper":
-            if(computerChoice ="rock") {
-                console.log("You lose")
-            }
-            break;
-        case "scissors":
-            if(computerChoice="paper") {
-                console.log("You win")
-            }
-            break;
-    }
-}
 
-console.log(playGame())
+function playRound (playerSelection, computerSelection) {
+    let result;
+     if (playerSelection=="rock" && computerSelection=="scissors") {
+        result ="You win! rock beats scissors"
+     }else if (playerSelection=="paper" && computerSelection =="rock") {
+        result ="You win! paper beats Rock"
+     }else if(playerSelection =="scissors" && computerSelection =="paper") {
+        result ="You win! scissors beats paper"
+     }else if(computerSelection =="rock" && playerSelection =="scissors") {
+      result ="You lose rock beats scissors "
+      }else if(computerSelection =="paper" && playerSelection =="rock") {
+         result ="You lose! paper beats rock"
+      }else if (computerSelection =="scissors" && playerSelection =="paper") {
+         result ="You lose scissors beats paper"
+      }else if (playerSelection == computerSelection) {
+        result ="Tie"}
+     return result;
+}  
+
+
+console.log(playRound(playerSelection,computerSelection))
+
+console.log(computerSelection)
+console.log(playerSelection)
