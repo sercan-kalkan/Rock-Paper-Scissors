6b12591ff23e71dfc6105c8dcb657baba16e881a
Sercan Kalkan <sercankalkan1@gmail.com>
2023-05-01 22:21:06 +0300

    variables that holds the score has been created

--- main.js
+++ main.js
@@ -8,34 +8,42 @@ let  getComputerChoice = function () {
 }
 
 const computerSelection = getComputerChoice();
-let playerScore;
-let computerScore;
+//variables saves the score of players
+let playerScore=0;
+let computerScore=0;
 
+//function for making choices and defining the winner
 function playRound (playerSelection, computerSelection) {
     let result;
      if (playerSelection=="rock" && computerSelection=="scissors") {
-        result ="You win! rock beats scissors"
+        result ="You win! rock beats scissors";
+         playerScore++;
      }else if (playerSelection=="paper" && computerSelection =="rock") {
         result ="You win! paper beats Rock"
+        playerScore++;
      }else if(playerSelection =="scissors" && computerSelection =="paper") {
         result ="You win! scissors beats paper"
+        playerScore++;
      }else if(computerSelection =="rock" && playerSelection =="scissors") {
-      result ="You lose rock beats scissors "
+         result ="You lose rock beats scissors "
+         computerScore++;
       }else if(computerSelection =="paper" && playerSelection =="rock") {
          result ="You lose! paper beats rock"
+         computerScore++;
       }else if (computerSelection =="scissors" && playerSelection =="paper") {
          result ="You lose scissors beats paper"
+         computerScore++;
       }else if (playerSelection == computerSelection) {
         result ="Tie"}
-     return result;
+     return `${result} Player score: ${playerScore}  Computer score: ${computerScore}`
 }  
 
-function game () {
-
+function game (playRound) {
       for (let i=0; i<=5; i++) {
-         
+
       }
 }
+
 
 console.log(playRound(playerSelection,computerSelection))
 
