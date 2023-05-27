2d617ce583f3ef13df5281844961d7eb3e661d90
Sercan Kalkan <sercankalkan1@gmail.com>
2023-05-28 00:04:04 +0300

    Game finished

--- main.js
+++ main.js
@@ -42,26 +42,45 @@ function playRound (playerSelection, computerSelection) {
         computerScore++;
      }else if (playerSelection == computerSelection) {
        result ="Tie"}
+
+      //show result after every game
+       const gameResult =document.getElementById('result');
+       gameResult.textContent =`${result}`
+
       //ending the game when someone's score is equal to 5
        if(playerScore>=5|| computerScore>=5) {
-         console.log('GAME OVER')
-         if (playerScore>computerScore) {
+         const gameOver = document.getElementById('gameOver');
+         gameOver.textContent='GAME OVER'
+         //when the game ends remove the buttons
+         const remove = document.getElementById('game')
+         remove.innerHTML='';
 
+         //when the game ends remove the alert for specific game
+         gameResult.innerHTML='';
+
+
+
+         //determine the winner
+         if (playerScore>computerScore) {
+            const winner =document.getElementById('winner');
+            winner.textContent="You Win!"
+            winner.style.color='green';
          }else if (computerScore>playerScore) {
-            console.log("You Loose")
+            winner.textContent="You Loose!"
+            winner.style.color='red';
          }else {
-            console.log('Tie')
+            winner.textContent="Tie!"
          }
        }
+
+
+
        //adding score to a div
        const playerScorePara = document.getElementById("playerScore");
        const computerScorePara = document.getElementById("computerScore");
    
        playerScorePara.textContent = `Player: ${playerScore}`;
        computerScorePara.textContent = `Computer: ${computerScore}`;
-
-
-       return  console.log(playerScore,computerScore, result);
    
  }
  
